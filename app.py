1121ba386ca1f457b7742c0213216c044773686a
Kelechi Igbokwe <kelechi_igbokwe@yahoo.com>
2018-07-30 15:48:32 +0200

    Completed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from flask_wtf import FlaskForm
 from wtforms import SelectField
-from bson.objectid import ObjectId
+from bson.objectid import ObjectId #for accesing objectid in mongodb
 
 def connect():
     connection = MongoClient('127.0.0.1', 27017)
@@ -22,9 +22,11 @@ class Form(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = Form()
+    #the for loops through th db where state = CA
     form.city.choices = [(city['_id'], city['cities']) for city in handle.location.find({"state":"CA"})]
     if request.method == 'POST':
         selected_city = form.city.data #returns the objectid of the selected city
+        #finds the city which has the objid
         city = handle.location.find({"_id" : ObjectId(selected_city)})[0]
         #print(city)
         #city = City.query.filter_by(id=form.city.data).first()
@@ -34,16 +36,17 @@ def index():
 
 @app.route('/city/<state>')
 def city(state):
+    #get cities given by the user
     cities = handle.location.find({"state":state})
     cityArray = []
     for city in cities:
         cityObj = {}
-        see = str(city["_id"])
-        cityObj['id'] = see
+        obj_id = str(city["_id"]) #objid cant be jsonified so we cob´nvert to string
+        cityObj['id'] = obj_id
         cityObj['state'] = city['state']
         cityObj['city'] = city['cities']
         cityArray.append(cityObj)
-        print(see)
+        #print(obj_id)
 
     return jsonify({'cities' : cityArray})
 
